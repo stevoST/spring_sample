8201750eff74cc0842c03f6233309d55a2c359d3
stevoST <stefan.starosta@gmail.com>
2019-03-11 22:44:45 +0100

    autowired configuration

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -3,14 +3,17 @@ import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
     @Bean(name = "customerService")
     public CustomerService getCustomerService() {
-        CustomerServiceImpl service = new CustomerServiceImpl(getCustomerRepository());
+//        CustomerServiceImpl service = new CustomerServiceImpl(getCustomerRepository());
+        CustomerServiceImpl service = new CustomerServiceImpl();
 //        service.setCustomerRepository(getCustomerRepository());
         return service;
     }
--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,6 +2,7 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -9,11 +10,18 @@ public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
 
+    public CustomerServiceImpl(){
+
+    }
+
     public CustomerServiceImpl(CustomerRepository customerRepository) {
+        System.out.println("We are using constructor injection");
         this.customerRepository = customerRepository;
     }
 
+    @Autowired
     public void setCustomerRepository(CustomerRepository customerRepository) {
+        System.out.println("We are using setter injection");
         this.customerRepository = customerRepository;
     }
 
