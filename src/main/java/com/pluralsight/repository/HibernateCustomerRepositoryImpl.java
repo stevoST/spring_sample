698a9ef81365dc2a5bd17278fac1b9b21255417e
DOMAIN\starosta <starosta@cngroup.dk>
2019-03-12 11:28:01 +0100

    xml config injection added annotation and removed setter for dbuser name using @Value

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -1,17 +1,15 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${dbUsername}")
     private String dbUsername;
-
-    public void setDbUsername(String dbUsername) {
-        this.dbUsername = dbUsername;
-    }
 
     @Override
     public List<Customer> findAll(){
