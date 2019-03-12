cde6b2448089426c78f90d6641d3b37ff2498ca7
DOMAIN\starosta <starosta@cngroup.dk>
2019-03-12 11:18:57 +0100

    xml config demo with db user

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -7,8 +7,16 @@ import java.util.List;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    private String dbUsername;
+
+    public void setDbUsername(String dbUsername) {
+        this.dbUsername = dbUsername;
+    }
+
     @Override
     public List<Customer> findAll(){
+        System.out.println(dbUsername);
+
         List<Customer> customers = new ArrayList<>();
 
         Customer customer = new Customer();
