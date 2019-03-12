7fe3d1b40a4c71acac4663b6a1e734e66c58cd74
DOMAIN\starosta <starosta@cngroup.dk>
2019-03-12 11:37:53 +0100

    added dbUsername with @Value using java config

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -5,10 +5,18 @@ import com.pluralsight.service.CustomerServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"com.pluralsight"})
+@PropertySource("app.properties")
 public class AppConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
 //    @Bean(name = "customerService")
 //    public CustomerService getCustomerService() {
